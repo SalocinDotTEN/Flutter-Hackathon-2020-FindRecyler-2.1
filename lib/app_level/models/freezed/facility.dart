5e3f41517a48ff2c39f5a743791ec9722300b5e5
aseem wangoo <aseemwangoo@gmail.com>
2020-06-28 15:22:35 +0800

    [C/Camera] Include center model

--- lib/app_level/models/freezed/facility.dart
+++ lib/app_level/models/freezed/facility.dart
@@ -16,3 +16,47 @@ abstract class FacilityModel with _$FacilityModel {
   factory FacilityModel.fromJson(Map<String, dynamic> json) =>
       _$FacilityModelFromJson(json);
 }
+
+class CenterFormData with ChangeNotifier {
+  String _address;
+
+  String get address => _address;
+
+  set address(String address) {
+    _address = address;
+    notifyListeners();
+  }
+
+  String _imgUrl;
+
+  String get imgUrl => _imgUrl;
+
+  set imgUrl(String imgUrl) {
+    _imgUrl = imgUrl;
+    notifyListeners();
+  }
+
+  String _name;
+
+  String get name => _name;
+
+  set name(String name) {
+    _name = name;
+    notifyListeners();
+  }
+
+  String _type;
+
+  String get type => _type;
+
+  set type(String type) {
+    _type = type;
+    notifyListeners();
+  }
+
+  @override
+  String toString() {
+    final _str = '${name} ${imgUrl} ${type} ${address}';
+    return _str;
+  }
+}
