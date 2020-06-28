c6894431cbdf18a0538fff232e32b91db18fed12
aseem wangoo <aseemwangoo@gmail.com>
2020-06-28 22:00:38 +0800

    [C/Rewards] Save progress

--- lib/rewards/rewards.dart
+++ lib/rewards/rewards.dart
@@ -9,16 +9,36 @@ class Rewards extends StatelessWidget {
     //
     return Scaffold(
       appBar: AppBar(title: Text('Rewards')),
+      backgroundColor: Colors.greenAccent,
       body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Rotator(
-              buttonText: '150',
-              onTap: () => Navigator.of(context).pop(),
+            ListView.builder(
+              shrinkWrap: true,
+              padding: EdgeInsets.only(bottom: 40, top: 10),
+              itemCount: 1,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) => _buildListItem(index),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildListItem(int index) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 18.0,
+      ),
+      child: Card(
+        child: ListTile(
+          leading: FlutterLogo(),
+          title: Text('One-line with both widgets'),
+          trailing: Icon(Icons.more_vert),
         ),
       ),
     );
